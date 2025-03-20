6ffea3cfeb45129420fdcb9c2dd2ea1907c9bfb1
lxwang12 <lxwang12@iflytek.com>
2025-03-20 16:34:48 +0800

    init

---
+++ map_mcp.py
@@ -0,0 +1,15 @@
+from mcp.server.fastmcp import FastMCP
+
+mcp = FastMCP("map")
+
+@mcp.tool()
+async def navigate_to(city: str) -> str:
+    """navigate to the city.
+
+    Args:
+        city: destination city name
+    """
+    return f"navigate to {city} success."
+
+if __name__ == "__main__":
+    mcp.run(transport='stdio')
